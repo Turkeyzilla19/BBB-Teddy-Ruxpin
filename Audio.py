5cfc17998d16dbda49693a86fac04e67e80d2be4
Turkeyzilla19 <adrianaaron19@hotmail.com>
2017-07-17 20:09:13 -0400

    Update Audio.py

--- Audio.py
+++ Audio.py
@@ -1,1 +1,17 @@
+import alsaaudio as aa
+import audioop
+from time import sleep
+import struct
+import math
+import array
+import numpy as np
+import wave
+import os
+import subprocess
 
+class AudioPlayer:
+  def __init__(self):
+    self.prevAudiovalue = 0
+    self.mouthValue = 0
+    
+  def play(self,fileName):
