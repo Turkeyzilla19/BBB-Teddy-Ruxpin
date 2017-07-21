4fdd7872727509766bbbe4aac4ea841d947c7b52
Turkeyzilla19 <adrianaaron19@hotmail.com>
2017-07-21 15:16:59 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,12 +8,14 @@ from chippyRuxpin_audioPlayer import AudioPlayer
 from chippyRuxpin_webFramework import WebFramework
 import Adafruit_BBIO.GPIO as GPIO
 
+words = raw_input("what words do you want? ")
+
 enM = "P8_7"
 enE = "P8_9"
 MOUTH_OPEN = "P8_8"
 MOUTH_CLOSE = "P8_10"
-ain3Pin = "P8_12"
-ain4Pin = "P8_14"
+EYES_OPEN = "P8_12"
+EYES_CLOSE = "P8_14"
 
 GPIO.setup(enM, GPIO.OUT)
 GPIO.setup(enE, GPIO.OUT)
@@ -48,5 +50,27 @@ def updateMouth():
                 GPIO.output( MOUTH_CLOSE, GPIO.HIGH)
         else:
             if( time.time() - lastMouthEventTime > 0.4 ):
-                GPIO.output( MOUTH_OPEN, 0 )
-                GPIO.output( MOUTH_CLOSE, 0 )
+                GPIO.output( MOUTH_OPEN, GPIO.LOW )
+                GPIO.output( MOUTH_CLOSE, GPIO.LOW )
+def updateEyes():
+    while isRunning:
+        GPIO.output( EYES_OPEN, GPIO.LOW)
+        GPIO.output( EYES_CLOSE, GPIO.HIGH)
+        time.sleep(0.4)
+        GPIO.output( EYES_CLOSE, GPIO.LOW)
+        GPIO.output( EYES_OPEN, GPIO.HIGH)
+        time.sleep(0.4)
+        GPIO.output( EYES_OPEN, GPIO.LOW)
+        GPIO.output( EYES_CLOSE, GPIO.HIGH)
+        time.sleep(0.4)
+        GPIO.output( EYES_CLOSE, GPIO.LOW)
+        GPIO.output( EYES_OPEN, GPIO.HIGH)
+        time.sleep(randint( 0,7))
+
+mouthThread = Thread(target=updateMouth)
+mouthThread.start()
+eyesThread = Thread(target=updateEyes)
+eyesThread.start()     
+audio = AudioPlayer()
+
+        
